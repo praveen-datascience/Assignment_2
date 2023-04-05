705aca6d91dd80c857a1deec954c808cf2576b94
praveen-datascience <125889920+praveen-datascience@users.noreply.github.com>
2023-04-05 15:35:16 +0100

    Update index.py
    did some necessary changes

--- index.py
+++ index.py
@@ -1,27 +1,37 @@
-
 # -*- coding: utf-8 -*-
 """
 Created on Sat Apr  3 07:30:29 2023
 @author: praveen
 """
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 def read_my_excel(filename):
-     """ This function is used to read excel file and return dataframe"""
+    """ This function is used to read excel file and return dataframe """
     excel_result = pd.read_excel(filename)
     return excel_result
+
+def  get_years_countries(dataframe):
+    # Split the DataFrame into two separate DataFrames, one with years and another with countries
+    df_countries = dataframe.iloc[1:, 0:1]  # select the first column containing years
+    df_years =   dataframe.iloc[:, 4:]  # select all columns starting from 4th column
+    return df_countries,df_years
 
 def filter_desired_columns(dataframe_name,column_names):
     """ This function is used to filter the desired columns"""
     return dataframe_name.loc[:, column_names]
 
 def filter_desired_rows(dataframe_name,row_locations):
-    """ This function is used to filter the desired rows and rounding data  to 4 figures"""
+    """ This function is used to filter the desired rows and rounding data  to 4 figures """
     return dataframe_name.iloc[row_locations].round(4)
 
+def fill_missing_val(dataframe_name,colname):
+    """ This function is used to fill missing values with it's mean value """
+    dataframe_name[colname].fillna(dataframe_name[colname].mean(), inplace=True)
+
 def plot_bar_chart(dataframe,index_name,title,xlabel,ylabel):
-    """ This function is used to plot bar chart"""
+    """ This function is used to plot bar chart """
     # set the index of the dataframe to 'Country Name' column
     dataframe = dataframe.set_index(index_name)
     # plot the bar chart
@@ -44,10 +54,14 @@ def plot_bar_chart(dataframe,index_name,title,xlabel,ylabel):
 df_forest = read_my_excel("forest_area_updated.xls")
 df_co2emission = read_my_excel("co2_emission_updated.xls")
 
+#one with years and another with countries
+country_df,years_df = get_years_countries(df_forest)
+
+
+
 #check first 5 rows
 #print(df_forest.head())
 #print(df_co2emission.head())
-
 #using describe method try to analyze the whole statistical data
 print(df_forest.describe())
 print(df_co2emission.describe())
@@ -56,11 +70,23 @@ print(df_co2emission.describe())
 df_forest = filter_desired_columns(df_forest,['Country Name', 'Country Code', '1991', '1995', '1999'])
 df_co2emission = filter_desired_columns(df_co2emission,['Country Name', 'Country Code', '1991', '1995', '1999'])
 
-# read forest data  only 1 African country, Australiya, Asian country and South American country 
+# read forest data  only for few countries  
 df_forest_specific_rows = filter_desired_rows(df_forest,[4,13,17, 29])
 
-# read co2 emmsion only 1 African country, Australiya, Asian country and South American country 
+# read co2 emmsion only for few countries 
 df_emmision_specific_rows = filter_desired_rows(df_co2emission,[4,13,17, 29])
+
+# fill missing values with column mean
+fill_missing_val(df_forest,'1991')
+fill_missing_val(df_forest,'1995')
+fill_missing_val(df_forest,'1999')
+
+fill_missing_val(df_co2emission,'1991')
+fill_missing_val(df_co2emission,'1995')
+fill_missing_val(df_co2emission,'1999')
+
+
+
 
 #display barchart for forest area data
 plot_bar_chart(df_forest_specific_rows,'Country Name','Forest Area by Country and Year','Country and Year','Forest area (% of land area)')
